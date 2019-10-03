9c316435c5babbaa32c868bc8c09f8b9f1380070
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-10-04 00:23:31 +0530

    Adding neon stylings
    Adding animations and styling

--- src/Board.js
+++ src/Board.js
@@ -98,7 +98,14 @@ class Board extends Component {
 
     // if the game is won, just show a winning msg & render nothing else
     if(this.state.hasWon){
-      return <h1>YOU WON !</h1>
+      return(
+        <div className="Board-title">
+          <div className="winner">
+            <span className="neon-orange">YOU</span>
+            <span className="neon-blue">WIN!</span>
+          </div>
+        </div>
+      );
     }
 
     // TODO
@@ -122,11 +129,19 @@ class Board extends Component {
       tblBoard.push(<tr>{row}</tr>);
     }
     return(
-      <table className="Board">
-        <tbody>
-          {tblBoard}
-        </tbody>
-      </table>
+      <div>
+        <div className="Board-title">
+          <div className="neon-orange">Lights</div>
+          <div className="neon-blue">Out</div>
+        </div>
+
+        <table className="Board">
+          <tbody>
+            {tblBoard}
+          </tbody>
+        </table>
+      </div>
+
     )
   }
 }
